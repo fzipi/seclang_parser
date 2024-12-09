6e1f033cf95b8b91ab36387281214ecab8beddaa
Felipe Zipitria <felipe.zipitria@owasp.org>
2024-12-08 22:19:56 -0300

    feat: add python classes
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,25 +1,10 @@
-from seclang_parser.SecLangParser import SecLangParser
-from seclang_parser.SecLangParserListener import SecLangParserListener
-from seclang_parser.parser import parse_file, parse_file_with_implementation
-
-
-class SecLangListener(SecLangParserListener):
-    def enterStmt(self, ctx):
-        print("Entering statement:", ctx.getText())
-
-    def enterAction(self, ctx: SecLangParser.ActionContext):
-        print("Entering action:", ctx.getText())
-
-    def enterComment(self, ctx: SecLangParser.CommentContext):
-        print("Entering comment:", ctx.getText())
-
-    def enterId(self, ctx: SecLangParser.IdContext):
-        print("Entering id:", ctx.getText())
+from seclang_parser.parser import parse_file
 
 
 def test_parse_file(crs_files):
     for file in crs_files:
+        print("Parsing file:", file)
         with open(file, "r") as f:
             s = f.read()
-            parse_file_with_implementation(s, SecLangListener())
-            assert True
+
+            parse_file(s)
